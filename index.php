05cc13554b8e2c784e3f451d7995d2515df64e6c
MariaPaulaS <mariathecharmix@hotmail.com>
2021-03-25 19:52:03 -0500

    Inicialización del proyecto y pagina index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Esto es un juego web">
+    <meta name="author" content = "Maria Paula Sabogal Motta">
+    <title>Juego web</title>
+</head>
+<body>
+        <h1>¡Bienvenido! Empieza a jugar </h1>
+        <h3>Esta es una prueba</h3>
+</body>
+</html>
